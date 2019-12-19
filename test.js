bcc579cd6f2a4ca7f0d60535f56475326b40fd4a
matthew-hub <mateuszr430@gmail.com>
2019-12-19 12:20:44 +0100

    create test js file to execute json editor

---
+++ test.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function(){
+  json_editor.new("editor", {
+    theme: one_dark_pro,
+    width: "100%",
+    height: "100%",
+    fontSize: '15px'
+  });
+}, false);
