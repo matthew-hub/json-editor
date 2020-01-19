6d2ef1c7963d853d8c2e809333a8190ad84e7991
matthew-hub <mateuszr430@gmail.com>
2020-01-19 11:40:23 +0100

    create scrollbars. adding number in column lines

--- javascript/renderer.js
+++ javascript/renderer.js
@@ -4,12 +4,18 @@ var json_editor = (function(editor) {
   // RENDERER CONSTRUCTOR
   var Renderer = function(container, settings) {
     this.container = container;
-    this.settings = settings;
-    this.list_code_lines = [];
-    this.activeLine = 0;
     this.editor_content = this.createEditorContent();
-    this.fields = this.createEditorFields();
-    // this.fields.editor_scroll_view = this.createEditorScrollView()
+    this.settings = settings;
+    this.activeLine = 0;
+    this.scrollbars = {
+      is_mouse_down: false,
+      previous_mouse_position: 0,
+      previous_thumb_position: 0,
+    };
+    this.list_code_lines = [];
+    this.elements = this.createEditorFields();
+    this.createScrollBars();
+    // this.elements.editor_scroll_view = this.createEditorScrollView()
     this.createNewLineCode();
     // this.createEditorContentLineCode();
   };
@@ -34,7 +40,7 @@ var json_editor = (function(editor) {
         editor_cursor,
         // editor_content,
         editor_code_lines_c,
-        editor_scroll_view ,
+        editor_scroll_view,
         editor_code_rows: editor_code_lines_c.childNodes
       };
     },
@@ -66,8 +72,6 @@ var json_editor = (function(editor) {
       return editor_code_lines_c;
     },
 
-   
-
     // CREATE COLUMN OF CODE LINES
     createColumnOfLines: function() {
       var editor_column_lines = doc.createElement('div');
@@ -77,19 +81,21 @@ var json_editor = (function(editor) {
 
     // CREATE NEW LINE OF CODE
     createNewLineCode: function() {
-      // console.log('[CONTENT]', this.fields.editor_content);
+      // console.log('[CONTENT]', this.elements.editor_content);
 
       var editor_code_line = doc.createElement('div');
       editor_code_line.className = 'code_line';
-      // editor_code_line.style.padding = '2px';
-      editor_code_line.style.height = '22px';
+      editor_code_line.style.padding = '2px';
+      editor_code_line.style.height = 'auto';
       // editor_code_line.style.overflow = 'hidden';
       // editor_code_line.style.textOverflow = 'ellipsis';
       editor_code_line.style.whiteSpace = 'nowrap';
-      editor_code_line.style.display = 'inline-block'
+      editor_code_line.style.display = 'block';
+      editor_code_line.style.margin = 0;
       editor_code_line.style.minHeight = '22px';
       editor_code_line.style.cursor = 'text';
-      editor_code_line.style.width = '100%'
+      editor_code_line.style.width = '100%';
+      // editor_code_line.style.backgroundColor = '#121212'
       // editor_code_line.style.minWidth = '100%'
       // editor_content_code.id = this.listOfCodeLines.length +1;
       editor_code_line.addEventListener(
@@ -106,31 +112,40 @@ var json_editor = (function(editor) {
         }.bind(this),
         false
       );
-      // editor_content_code.addEventListener('select', function(e) {
-      //   // console.log(this)
-      //   // console.log('click');
-      //   // console.log('src', e.srcElement)
-      //   // this.setLineActiveChangeColor(e.srcElement)
-      //   // this.focusTextArea();
-      //   console.log('[SELECT]', );
 
-      //   // console.log('event', e) ;
-      // }.bind(this), false);
-
-      // editor_content_code.textContent = this.fields.text_area.value;
+      // editor_content_code.textContent = this.elements.text_area.value;
       // this.listOfCodeLines.push(editor_content_code);
       this.list_code_lines.splice(this.activeLine + 1, 0, editor_code_line);
 
       // APPENDCHILD WHEN FIRST TIME CREATE NEW LINE CODE
       if (this.list_code_lines.length <= 1) {
-        this.fields.editor_code_lines_c.appendChild(editor_code_line);
+        this.elements.editor_code_lines_c.appendChild(editor_code_line);
       } else {
         // INSERT NEW LINE IN RIGHT PLACE
-        this.list_code_lines[this.activeLine].parentNode.insertBefore(editor_code_line, this.list_code_lines[this.activeLine].nextSibling);
+        this.list_code_lines[this.activeLine].parentNode.insertBefore(
+          editor_code_line,
+          this.list_code_lines[this.activeLine].nextSibling
+        );
       }
       this.setLineActiveChangeColor(editor_code_line);
-    },
 
+      var editor_column_line = doc.createElement('div');
+      editor_column_line.className = 'code_column_line';
+      editor_column_line.style.padding = '2px';
+      editor_column_line.style.height = '22px';
+      editor_column_line.style.float = 'left';
+      editor_column_line.style.textAlign = 'right';
+      // editor_column_line.style.overflow = 'hidden';
+      // editor_column_line.style.textOverflow = 'ellipsis';
+      // editor_column_line.style.whiteSpace = 'nowrap';
+      editor_column_line.style.display = 'block';
+      editor_column_line.style.margin = 0;
+      // editor_column_line.style.minHeight = '22px'
+      editor_column_line.style.cursor = 'context-menu';
+      editor_column_line.style.width = '100%';
+      editor_column_line.innerHTML = this.list_code_lines.length;
+      this.elements.editor_column_lines.appendChild(editor_column_line);
+    },
 
     // CREATE CURSOR
     createCursor: function() {
@@ -138,7 +153,7 @@ var json_editor = (function(editor) {
       div.className = 'editor_cursor_box';
       // div.style.position = 'absolute';
       // div.style.float = 'left';
-  
+
       var editor_cursor = doc.createElement('div');
       div.appendChild(editor_cursor);
       //  editor_cursor.style.cursor = "text";
@@ -153,22 +168,38 @@ var json_editor = (function(editor) {
 
     createEditorScrollView: function(element) {
       var editor_scroll_view = doc.createElement('div');
-      editor_scroll_view.className = 'editor-scroll-view';
-      editor_scroll_view.style.position = 'relative';
-      editor_scroll_view.style.display = 'block';
-      // editor_scroll_view.style.overflow = 'hidden';
-      editor_scroll_view.style.overflowY = 'scroll';
-      editor_scroll_view.style.overflowX = 'scroll';
-      editor_scroll_view.style.width = 'auto';
-      editor_scroll_view.style.height = '100%';
-      editor_scroll_view.style.minHeight = '100%';
       // editor_scroll_view.style.top = '0'
-      // this.fields.editor_content.appendChild(editor_scroll_view);
-      // this.fields.editor_code_lines_c.appendChild(editor_scroll_view);
-      console.log('[this fields]', element);
+      // this.elements.editor_content.appendChild(editor_scroll_view);
+      // this.elements.editor_code_lines_c.appendChild(editor_scroll_view);
+      // console.log('[this elements]', element);
       // element.appendChild(editor_scroll_view)
       this.editor_content.appendChild(editor_scroll_view);
       return editor_scroll_view;
+    },
+
+    // CREATE SCROLLBARS
+    createScrollBars: function() {
+      var editor_scrollbar_y = doc.createElement('div');
+      editor_scrollbar_y.className = 'scrollbar-y';
+
+      var editor_scrollbar_x = doc.createElement('div');
+      editor_scrollbar_x.className = 'scrollbar-x';
+
+      var scrollbar_thumb_y = doc.createElement('div');
+      scrollbar_thumb_y.className = 'scrollbar-thumb';
+      var scrollbar_thumb_x = doc.createElement('div');
+      scrollbar_thumb_x.className = 'scrollbar-thumb';
+
+      editor_scrollbar_y.appendChild(scrollbar_thumb_y);
+      editor_scrollbar_x.appendChild(scrollbar_thumb_x);
+      // var scrollbar_thumb = doc.createElement('div');
+      // scrollbar_thumb.className = 'scrollbar_thumb'
+      // editor_scrollbar_h.appendChild(scrollbar_thumb)
+
+      this.elements.editor_scrollbar_y = editor_scrollbar_y;
+      this.elements.editor_scrollbar_x = editor_scrollbar_x;
+      this.elements.editor_scroll_view.appendChild(editor_scrollbar_y);
+      this.elements.editor_scroll_view.appendChild(editor_scrollbar_x);
     },
 
     // SET CODE LINE TO ACTIVE 'CHANGE COLOR'
@@ -177,7 +208,7 @@ var json_editor = (function(editor) {
       // console.log('[line]', this.listOfCodeLines[this.activeLine]);
       // console.log('list', this.listOfCodeLines);
       this.activeLine = this.list_code_lines.indexOf(element);
-      console.log(this.activeLine);
+      console.log('[active line]', this.activeLine);
       // element.className += ' active_line';
       element.style.backgroundColor = '#212121';
     },
@@ -186,7 +217,7 @@ var json_editor = (function(editor) {
     focusTextArea: function() {
       console.log('[FOCUS AREA]');
 
-      this.fields.text_area.focus();
+      this.elements.text_area.focus();
     },
 
     length: 0,
@@ -198,5 +229,4 @@ var json_editor = (function(editor) {
   };
 
   return editor;
-
 })(json_editor || {});
